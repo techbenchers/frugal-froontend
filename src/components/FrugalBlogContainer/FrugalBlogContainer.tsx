217cc90d85402ad62953f8ff982a07521dc04511
suyashdeshpande <suyash.champ1@gmail.com>
2020-04-18 18:06:04 +0530

    Updated blog component

---
+++ src/components/FrugalBlogContainer/FrugalBlogContainer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './FrugalBlogContainer.css';
+import {FrugalPostCard, FrugalPostCardProps} from "../FrugalPostCard/FrugalPostCard";
+import paella from "../../static/images/paella.jpeg";
+
+
+const frugal: FrugalPostCardProps = {
+    img: paella,
+    alt: "paella",
+    title: "Shrimp and Chorizo Paella",
+    date: "September 14, 2016",
+    body: "This impressive paella is a perfect party dish and a fun meal to cook together with your guests.",
+    author: "Aniket Singh"
+};
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export interface FrugalBlogContainerProps {
+
+}
+
+export interface FrugalBlogContainerState {
+
+}
+
+export class FrugalBlogContainer extends React.Component<FrugalBlogContainerProps, FrugalBlogContainerState> {
+    render() {
+        return (
+            <>
+                {
+                    list.map((i: number) => <FrugalPostCard key={i} {...frugal}/>)
+                }
+            </>
+        );
+    }
+}
